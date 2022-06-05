254f4ad0bd636de26a77127760d5f0a6fda001f7
shivam ivam <ivamanonymous@gmail.com>
2022-06-05 13:38:51 +0530

    drum kit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+var drumsCount=document.querySelectorAll(".drum").length;
+for(var i=0;i<drumsCount;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",drumSound);
+        }
+
+function drumSound(){
+makeSound(this.innerHTML);
+buttonAnimation(this.innerHTML);
+}
+
+document.addEventListener("keypress",function(event){
+
+makeSound(event.key);
+buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch(key){
+        case "w":
+          var audio=new Audio("sounds/crash.mp3");
+          audio.play(); 
+          break;
+          case "a":
+          var audio=new Audio("sounds/kick-bass.mp3");
+          audio.play(); 
+          break;
+          case "s":
+              var audio=new Audio("sounds/snare.mp3");
+              audio.play(); 
+              break;
+              case "d":
+                  var audio=new Audio("sounds/tom-1.mp3");
+                  audio.play(); 
+                  break;
+                  case "j":
+                      var audio=new Audio("sounds/tom-2.mp3");
+                      audio.play(); 
+                      break; case "k":
+                      var audio=new Audio("sounds/tom-3.mp3");
+                      audio.play(); 
+                      break;
+                      case "l":
+                          var audio=new Audio("sounds/tom-4.mp3");
+                          audio.play(); 
+                          break;
+                          default:
+                              break;
+    }
+}
+
+function buttonAnimation (currentKey){
+    document.querySelector("." + currentKey).classList.add("pressed");
+    
+     setTimeout(function(){
+         document.querySelector("." + currentKey).classList.remove("pressed");
+     },100);
+}
